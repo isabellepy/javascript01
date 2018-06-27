fa90c4d43fe6870df348ee076426a53e4f66eeb0
isabellepy <31375827+isabellepy@users.noreply.github.com>
2018-06-27 12:37:40 +0200

    Add files via upload

---
+++ exercise-shaver-sharp-html-master/src/shaver-sharp.js
@@ -0,0 +1,70 @@
+/*jshint esversion: 6 */
+
+/**
+ * shaverSharp module.
+ *
+ * @module src/shaver-sharp
+ * @author John Häggerud
+ * @author Mast Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+/**
+ * Creates the specified starting tag.
+ *
+ * @param {string} tagName tagName is a string that specifies the type of starting tag to be created.
+ * @returns {string} Returns a starting tag as a string.
+ */
+function createBeginTag (tagName) {
+  return `<${tagName}>`;
+}
+
+/**
+ * Creates the specified closing tag.
+ *
+ * @param {string} tagName tagName is a string that specifies the type of closing tag to be created.
+ * @returns {string} Returns a closing tag as a string.
+ */
+function createEndTag (tagName) {
+  return `</${tagName}>`;
+}
+
+/**
+ * Creates the specified element with content. If no content is specified a self-closing element will be created.
+ *
+ * @param {string} tagName tagName is a string that specifies the type of element to be created.
+ * @param {string=} [innerHTML = ''] innerHTML is a string that describes the element's descendants.
+ * @returns {string} Returns an element as a string.
+ */
+function createElement (tagName, innerHTML = '') {
+  return `<${tagName}>innerHTML</${tagName}>`;
+}
+
+/**
+ * Creates the specified elements with contents. If a tagName is not paired with a content a self-closing element
+ * will be created.
+ *
+ * @param  {Array.<Object<string, string>>} elementData elementData is an array of objects.
+ * @returns {string} Returns specified elements as a string.
+ */
+function createElements(elementData) {
+  let first = "";
+  let nameTag = "";
+  for(let i = 0; i < elementData.length; i++){
+    nameTag = Object.values(elementData[i]);
+    for(let j = 0; j < nameTag.length; j++){
+      if(j % 2 === 0) {
+        first += `<${nameTag[j]}>${nameTag[++j]}</${nameTag[--j]}>`;
+      }
+    }
+  }
+  return first;
+}
+
+// Exports
+exports.createBeginTag = createBeginTag;
+exports.createEndTag = createEndTag;
+exports.createElement = createElement;
+exports.createElements = createElements;
