271301ffce85148ee1643a9d8a51a1dd18ac8d84
isabellepy <31375827+isabellepy@users.noreply.github.com>
2018-06-28 11:50:08 +0200

    Add files via upload

---
+++ exercise-better-than-average-master/src/my-array.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6 */
+
+/**
+ * my-array module.
+ *
+ * @module src/my-array
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.1.0
+ */
+
+'use strict'
+
+/**
+ * Returns the name of the students whose points is greater than,
+ * or equal to, the average of the points.
+
+ * @param {object[]} students
+ * @returns {string[]}
+ */
+function filterBetterThanAverage(students) {
+  let average = students.reduce(function(sum, people){
+    return sum + people.points;
+  }, 0) / students.length;
+
+  return students.filter(p => p.points >= average).map(p => p.name);
+}
+
+exports.filterBetterThanAverage = filterBetterThanAverage;
